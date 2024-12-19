796a1f23bca5e9393e3b78f8cb745ea2885fcb1c
Sam-Fatehmanesh <sam.fatehmanesh@gmail.com>
2024-12-19 07:22:52 -0800

    Changed memory to 96 from 128Gi

--- worker/worker.py
+++ worker/worker.py
@@ -49,10 +49,10 @@ def get_spark(**kwargs):
     spark = (
         SparkSession.builder.master("local[*]")
         .config("spark.driver.maxResultSize", "0")
-        .config("spark.executor.memory", "70g")
-        .config("spark.driver.memory", "70g")
+        .config("spark.executor.memory", "80g")
+        .config("spark.driver.memory", "80g")
         .config("spark.memory.offHeap.enabled", True)
-        .config("spark.memory.offHeap.size", "70g")
+        .config("spark.memory.offHeap.size", "80g")
         .appName("sampleCodeForReference")
         .getOrCreate()
     )
